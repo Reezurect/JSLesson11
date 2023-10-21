ead8c4c867b3345176459d2802cdf44f15c7c696
Denis Zudilin <zudilin2014@yandex.ru>
2023-10-22 01:27:35 +0300

    пробник

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+'use strict'
 
+const btn = document.getElementById('btn');
 
+btn.addEventListener('click', function () {
+    const square = document.querySelector('square');
+    square.style.backgroundColor = 'green';
+});
 
-
-
+document.addEventListener("DOMContentLoaded", eCloseclick());
+document.getElementById('btn').addEventListener("click", eCloseclick);
+function eCloseclick() {
+    let x = document.getElementById('e_btn');
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"
+    }
+};
